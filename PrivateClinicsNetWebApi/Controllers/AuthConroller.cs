bdbef6c435b166b55c2aade90724bc2683b0f962
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2024-12-13 23:18:03 +0200

    Implememnt AuthConroller

--- PrivateClinicsNetWebApi/Controllers/AuthConroller.cs
+++ PrivateClinicsNetWebApi/Controllers/AuthConroller.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PrivateClinicsWebNet.DataAccess;
+using PrivateClinicsWebNet.DataAccess.DTOs;
 
 namespace PrivateClinicsNetWebApi.Controllers
 {
@@ -8,5 +9,38 @@ namespace PrivateClinicsNetWebApi.Controllers
     public class AuthConroller : ControllerBase
     {
         private readonly AuthService _authService;
+
+        public AuthConroller(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            try
+            {
+                await _authService.Register(model.Email, model.Password, model.Role);
+                return Ok("User registered successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto model)
+        {
+            try
+            {
+                string token = await _authService.Login(model.Email, model.Password);
+                return Ok(new { Token = token});
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
--- PrivateClinicsWebNet.DataAccess/Services/AuthService.cs
+++ PrivateClinicsWebNet.DataAccess/Services/AuthService.cs
@@ -11,7 +11,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLogic.Services
+namespace PrivateClinicsWebNet.DataAccess
 {
     public class AuthService
     {
