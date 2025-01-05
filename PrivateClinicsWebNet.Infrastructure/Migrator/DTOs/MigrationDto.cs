2bb82b1b7053563b082e39643e27e705521a48ab
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-01-05 23:23:46 +0200

    Add migration dto

--- PrivateClinicsWebNet.Infrastructure/Migrator/DTOs/MigrationDto.cs
+++ PrivateClinicsWebNet.Infrastructure/Migrator/DTOs/MigrationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrivateClinicsWebNet.BusinessLogic.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace PrivateClinicsWebNet.Infrastructure.Migrator.DTOs
 {
-    internal class MigrationDto
+    public record MigrationDto
     {
+        public Doctor Doctor { get; set; }
+        public List<Patient> PatientsList { get; set; }
     }
 }
